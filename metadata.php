72506dcc98ba7e4933ed88dd9a1e44672b602f60
FC-Sebastian <112632211+FC-Sebastian@users.noreply.github.com>
2024-04-19 15:52:26 +0200

    Initial commit

---
+++ metadata.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => '',
+    'title'       => [
+        'de' => '',
+        'en' => ''
+    ],
+    'description' => [
+        'de' => '',
+        'en' => ''
+    ],
+    'version'     => '',
+    'author'      => '',
+    'blocks'      => [
+        [
+            'template' => '',
+            'block'    => '',
+            'file'     => ''
+        ],
+    ],
+    'extend' => [
+    ],
+    'controllers' => [
+    ],
+    'templates' => [
+    ]
+];
